07cc41c8fbc6d431275cbfce99d74e0c426d3354
Bishwa <93580657+Bishwa-b@users.noreply.github.com>
2026-01-27 13:28:22 +0530

    Delete src directory

--- src/components/Home/lib/utils.js
+++
@@ -1,8 +0,0 @@
-/**
- * Combine class names conditionally.
- * @param {...string} classes
- * @returns {string}
- */
-export function cn(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
