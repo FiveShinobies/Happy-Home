84542a08774de90e86deaab0dc1819f3eda6c016
Amol Shrivastava <amolshrivastava596@gmail.com>
2026-02-02 22:36:43 +0530

    aws url added

--- happy-home/src/api/api.js
+++ happy-home/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://44.210.134.103",
 });
 
 // 🔑 REQUEST INTERCEPTOR (TOKEN ATTACHED HERE)
